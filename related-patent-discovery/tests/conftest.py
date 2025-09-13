97dcdf37c5f90ee95745940a573d0716c98f7f45
ChamodiLiyanage <it21828966@my.sliit.lk>
2025-09-13 23:28:46 +0530

    CI test

--- related-patent-discovery/tests/conftest.py
+++ related-patent-discovery/tests/conftest.py
@@ -5,7 +5,7 @@ import pytest_asyncio
 from httpx import AsyncClient
 from httpx import ASGITransport
 
-# Fix path so 'app' can be found
+# Fix path so 'app' can be founddd
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 
 from app import app
